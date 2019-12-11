dabf3fff0456074452002a547a89fbbe4dc10981
Reuben Remy <reubenremy@gmail.com>
2019-12-11 17:43:58 -0500

    pg-promise query successes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const db = require('./conn.js');
 
-db.query('SELECT * FROM personal')
+// db.query('SELECT * FROM personal')
+// .then(response => 
+//     response.map(each => {console.log(each.task, each.status)})
+//  ); 
+
+db.query("INSERT INTO personal (task, status) VALUES ('pg-promise success', true) ")
 .then(response => 
     response.map(each => {console.log(each.task, each.status)})
  ); 
