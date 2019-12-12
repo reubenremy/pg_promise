3091d3a15fa0f2672cc87b7835fe5cc5bbf4b4bb
Reuben Remy <reubenremy@gmail.com>
2019-12-12 10:46:04 -0500

    Small Updates

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const db = require('./conn.js');
 // db.query("INSERT INTO personal (task, status) VALUES ('pg-promise success', true) ")
 //     .then(response => response.map(res => {console.log(res.task, res.status)}));
 
-db.query("select * from development")
+db.query("select * from personal")
     .then(res => {
-        // console.log(res[0]);
+        // console.log(res[0]); 
         // console.log(res[1]);
         // console.log(res[2]);
+        console.log(res); //print all Tasks as .JSON object
     });
 
