d5fcbdba768456bfcd99dd4d4f6bd1f886eb6acc
Reuben Remy <reubenremy@gmail.com>
2019-12-12 07:15:48 -0500

    Refactored

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const db = require('./conn.js');
 //     response.map(each => {console.log(each.task, each.status)})
 //  ); 
 
-db.query("INSERT INTO personal (task, status) VALUES ('pg-promise success', true) ")
-.then(response => 
-    response.map(each => {console.log(each.task, each.status)})
-);
+// db.query("INSERT INTO personal (task, status) VALUES ('pg-promise success', true) ")
+//     .then(response => response.map(res => {console.log(res.task, res.status)}));
+
+db.query("select * from development")
+    .then(res => {
+        // console.log(res[0]);
+        // console.log(res[1]);
+        // console.log(res[2]);
+    });
+
